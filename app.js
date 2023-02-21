0062cce56e028172398b2580411a6da3f508b3cf
Payam <payam.koohii@gmail.com>
2023-02-21 14:59:03 +0330

    Test1

---
+++ app.js
@@ -0,0 +1,9 @@
+function handleEditProfile() {
+    document.getElementById("edit-profile").style.display = "block"
+    document.getElementById("user-profile").style.display = "none"
+}
+
+function handleUpdateProfile() {
+    document.getElementById("edit-profile").style.display = "none"
+    document.getElementById("user-profile").style.display = "block"
+}
